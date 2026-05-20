ac9b308133a01dda7067c8ef95f1e98c70f9b0d6
VimukthiPramudantha <vimukthipramudanth@gmail.com>
2026-05-21 00:36:12 +0530

    Force device in model load

--- yolo.py
+++ yolo.py
@@ -1,11 +1,9 @@
 from ultralytics import YOLO
 
-# Load latest lightweight model
 model = YOLO("yolo11n.pt")
 
-# Run webcam detection
 model.predict(
     source=0,
-    show=True,
-    device=0
+    device=0,   # GPU
+    show=True
 )
